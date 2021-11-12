af150a7d0fc8c030ec9e6ff14b06021bc38ce664
Iryna Kresinska <kresi.ira@gmail.com>
2021-11-12 12:10:49 +0100

    initial resume markup
    header + two columns containing basic sections

--- t-cv-client/src/App.js
+++ t-cv-client/src/App.js
@@ -6,12 +6,33 @@ function App() {
       <header className="header">
         Header
       </header>
-      <article>
-          Article
-      </article>
-      <footer>
-          Footer
-      </footer>
+      <div className="content-container">
+          <article>
+              <section>
+                  <h2>Resume objective/ resume summary</h2>
+              </section>
+              <section>
+                  <h2>Experience</h2>
+              </section>
+              <section>
+                  <h2>Education</h2>
+              </section>
+              <section>
+                  <h2>Certificates</h2>
+              </section>
+          </article>
+          <article>
+              <section>
+                  <h2>Personal Info</h2>
+              </section>
+              <section>
+                  <h2>Skills</h2>
+              </section>
+              <section>
+                  <h2>Interests</h2>
+              </section>
+          </article>
+      </div>
     </main>
   );
 }
