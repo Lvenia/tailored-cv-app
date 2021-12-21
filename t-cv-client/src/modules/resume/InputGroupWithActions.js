7cf4f06dfa618bf97a46f7ac919d81c9075549e3
Iryna Kresinska <kresi.ira@gmail.com>
2021-12-21 15:13:21 +0100

    add bullet points to local state, render bullets from local state

--- t-cv-client/src/modules/resume/InputGroupWithActions.js
+++ t-cv-client/src/modules/resume/InputGroupWithActions.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextInput from './TextInput';
-import { getKeys, INPUT_DEFINITIONS } from './consts';
+import { generateId, getKeys, INPUT_DEFINITIONS } from './consts';
 import Button from '../../components/Button';
+import TextInputWithAction from './TextInputWithAction';
+import { addEntry } from './actionHandlers';
 
 const InputGroupWithActions = ({
   name,
@@ -9,9 +11,20 @@ const InputGroupWithActions = ({
   onCancel,
   handleAction
 }) => {
+  const [bullets, setBullets] = useState([]);
+  console.log(bullets);
 
   const groupIsEdited = editedSectionName === name;
   const { fieldsetLabel, inputs } = INPUT_DEFINITIONS[name];
+  const { bulletPoints } = inputs;
+
+  const addBulletPoint = (name, value) => {
+    const item = {
+      id: generateId(),
+      value: value
+    };
+    setBullets(prev => prev.concat(item));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -38,19 +51,59 @@ const InputGroupWithActions = ({
   const renderInputGroup = (sectionName) => {
     const keys = getKeys(INPUT_DEFINITIONS[sectionName].inputs);
     return keys.map(key => {
-      let { ref } = INPUT_DEFINITIONS[sectionName].inputs[key];
-      let { label } = INPUT_DEFINITIONS[sectionName].inputs[key];
-      let { name } = INPUT_DEFINITIONS[sectionName].inputs[key];
-      return (
-        <TextInput
-          key={name}
-          inputRef={ref}
-          label={label}
-          name={name}
-          submitHandler={handleSubmit}
-        />
-      );
-    });
+        if (key !== 'bulletPoints') {
+          let { ref } = INPUT_DEFINITIONS[sectionName].inputs[key];
+          let { label } = INPUT_DEFINITIONS[sectionName].inputs[key];
+          let { name } = INPUT_DEFINITIONS[sectionName].inputs[key];
+          return (
+            <TextInput
+              key={name}
+              inputRef={ref}
+              label={label}
+              name={name}
+              submitHandler={handleSubmit}
+            />
+          );
+        }
+
+        if (key === 'bulletPoints') {
+          const { label, name, ref } = INPUT_DEFINITIONS[sectionName].inputs.bulletPoints;
+          return (
+            <>
+              <div className="add-bullets">
+                <TextInputWithAction
+                  textarea={true}
+                  name={name}
+                  label={label}
+                  inputRef={ref}
+                  handleAction={addBulletPoint}
+                  // onCancel={handleCancel}
+                  editedSectionName={editedSectionName}
+                />
+              </div>
+              <div className="bullets">
+                {bullets.length > 0 && (
+                  <ul>
+                    {bullets.map(bullet => {
+                      return (
+                        <li key={bullet.id}>
+                          <p>{bullet.value}</p>
+                          <div className="btn-row">
+                            <button>+</button>
+                            <button>E</button>
+                            <button>-</button>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                )}
+              </div>
+            </>
+          );
+        }
+      }
+    );
   };
 
   return (
